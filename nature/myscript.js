c837e5616649681107de52c0c154086eec6f60d2
pb-fordham <142920891+pb-fordham@users.noreply.github.com>
2025-12-01 14:33:45 -0500

    Add files via upload

--- nature/myscript.js
+++ nature/myscript.js
@@ -4,9 +4,35 @@ $(".purpleleaf").hover(function(){
     $(".yellowleaf").fadeIn(3000);
     $("h1").fadeIn(1000);
     $(".greenleaf").fadeIn(5000);
+}, function(){
+    $(".yellowleaf").fadeOut("fast");
+    $(".greenleaf").fadeOut("fast");
+
 });
 
 
+
+$(".rose").hover(function(){
+    $(".rosetext").fadeToggle();
+    $(".daisy").fadeToggle();
+    $(".rose2").fadeToggle();
+    $(".rose3").fadeToggle();
+});
+
+$(".daisy").hover(function(){
+    $(".daisytext").fadeToggle();
+    $(".rose").fadeToggle();
+    $(".daisy2").fadeToggle();
+});
+
+
+$(".burger").click(function(){
+    $(".navigation").fadeToggle();
+});
+
+$("a").click(function(){
+    $(".navigation").hide();
+});
 
 
 
